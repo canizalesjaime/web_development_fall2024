44e4efac170a2dd41c0d3fda41e08a4050043963
canizalesjaime <canizales.jaime16@gmail.com>
2024-12-09 18:16:38 -0500

    react props

---
+++ clazz_12_react/react-props/src/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//1. Apply CSS styles to App.jsx component
+//to match the appearance on the completed app:
+//https://c6fkx.csb.app/
+//2. Extract the contact card as a reusable Card component.
+//3. Use props to render the default Beyonce contact card.
+//4. Use the contacts.js file to create card components.
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
